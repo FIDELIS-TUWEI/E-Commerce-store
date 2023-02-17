031249d36c2109b76dd92db56e58469a00b9569c
Fidelis <fideliofidel9@gmail.com>
2023-02-17 07:28:28 +0300

    Data Fetched from Firebase

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
+import Firebase from '../firebase'
+
 const Home = () => {
     return ( 
-        <></>
+        <>
+            <Firebase />
+        </>
      );
 }
  
--- src/firebase.js
+++ src/firebase.js
@@ -5,29 +5,49 @@ import {
     getFirestore 
 } from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCsLoBiTYPq-305XSzfN0DbZpfoC4bHfCc",
-  authDomain: "fleekyffect.firebaseapp.com",
-  projectId: "fleekyffect",
-  storageBucket: "fleekyffect.appspot.com",
-  messagingSenderId: "290744464462",
-  appId: "1:290744464462:web:d76f511b14d24ba37e7f1f",
-  measurementId: "G-5DJBGRB05D"
-};
+const Firebase = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCsLoBiTYPq-305XSzfN0DbZpfoC4bHfCc",
+        authDomain: "fleekyffect.firebaseapp.com",
+        projectId: "fleekyffect",
+        storageBucket: "fleekyffect.appspot.com",
+        messagingSenderId: "290744464462",
+        appId: "1:290744464462:web:d76f511b14d24ba37e7f1f",
+        measurementId: "G-5DJBGRB05D"
+      };
+      
+      // Initialize Firebase
+      initializeApp(firebaseConfig);
+      
+      // init services
+      const db = getFirestore()
+      
+      
+      //collection ref
+      const colRef = collection(db, "jumia_products")
+      
+      
+      //get collection data
+      getDocs(colRef)
+          .then((snapshot) => {
+              let jumia_products = []
+              snapshot.docs.forEach((doc) => {
+                jumia_products.push({...doc.data, id: doc.id})
+              })
+              console.log(jumia_products)
+          })
+          .catch(err => {
+            console.log(err.message)
+          })
 
-// Initialize Firebase
-initializeApp(firebaseConfig);
-
-// init services
-const db = getFirestore()
 
 
-//collection ref
-const colRef = collection(db, "jumia_products")
+    return (
+        <div></div>
+    )
+}
 
 
-//get collection data
-getDocs(colRef)
-    .then((snapshot) => {
-        console.log(snapshot.docs)
-    })
+
+
+export default Firebase;
