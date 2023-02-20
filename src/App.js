a6b3e54f445452a9c29d8787b72630f217366d2d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-20 06:10:35 +0300

    BrowserRouter created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import Nav from './components/Nav';
-import './App.css'
-import Products from './pages/Products';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
+import './App.css'
+
+// pages
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+// layout
+import RootLayout from './layouts/RootLayout';
+
+
+// router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='login' element={<Login />} />
+      <Route path='signup' element={<SignUp />} />
+      <Route path='products' element={<Products />} />
+    </Route>
+  )
+)
 
 const App = () => {
   return ( 
     <div className="App">
-      <Nav />
-      <Products />
+      <RouterProvider router={router} />
     </div>
    );
 }
