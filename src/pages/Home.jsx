188d343a0b9a70f06b0b9c4c450383bbc9cbe447
Fidelis <114683429+FIDELIS-TUWEI@users.noreply.github.com>
2023-02-22 15:16:39 +0300

    homepage title created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 const Home = () => {
     return ( 
         <>
-            <div>
-                <h2>Home Page</h2>
+            <div className="home__section">
+                <h2>Welcome to Fleekyffect</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
             </div>
             
