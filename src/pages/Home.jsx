94ef94d54a6edb64ef104cbedb6c21345dee001d
Fidelis <114683429+FIDELIS-TUWEI@users.noreply.github.com>
2023-02-22 15:04:39 +0300

    footer copyright

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ const Home = () => {
                 <h2>Home Page</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
             </div>
+            
+            <footer>
+                <p>&copy; All rights reserved. Fleekyffect-2023</p>
+            </footer>
         </>
      );
 }
