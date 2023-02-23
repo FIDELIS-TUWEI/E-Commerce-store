db5d54acb99997bdfc2d458518f5904569d05f93
Fidelis <114683429+FIDELIS-TUWEI@users.noreply.github.com>
2023-02-23 08:04:49 +0300

    route chnages for error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,13 @@
-import {useRouteError} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  const error = useRouteError();
   
   return (
-    <div id="error-page">
+    <div className="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occured. </p>
+    
+      <p>Go to the <Link to="/">HomePage</Link>.</p>
     </div>
   );
   
