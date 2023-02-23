6733cd926791a905a6facda5af96e1896f3c4336
Fidelis <114683429+FIDELIS-TUWEI@users.noreply.github.com>
2023-02-23 14:07:10 +0300

    logout state created

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,29 @@
+import { userAuth } from '../context/AuthContext
+import { useNavigate } from 'react-router-dom'
+
 const Account = () => {
+  // destructure user from userAuth
+  const {user, logout} = userAuth();
+  
+  // useNavigate after logout
+  const navigate = useNavigate();
+  
+  // handlelogout
+  const handleLogout = async () => {
+    try {
+      await logout ();
+      navigate("/");
+      window.alert("Succesfully logged out");
+    } catch (error) => {
+      console.log(error.message);
+    }
+  }
+  
   return (
     <div>
         <h1>Account</h1>
-        <p>User Logged in: </p>
-        <button>Logout>
+        <p>User Logged in: { user && user.email }</p>
+        <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
