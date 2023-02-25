b14d846f43192a2945b96430957222fc1db51f2f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-25 06:30:29 +0300

    handleRegister function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -27,6 +27,7 @@ export const AuthProvider = ({children}) => {
     return unsubscribe
   }, [])
 
+  // value
   const value = {
     currentUser,
     signUp
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,31 +4,40 @@ import { useAuth } from "../context/AuthContext";
 
 const SignUp = () => {
     // useState
-    const [signUpPassword, setSignUpPassword] = useState("")
     const [signUpEmail, setSignUpEmail] = useState("")
+    const [signUpPassword, setSignUpPassword] = useState("")
+    const { SignUp } = useAuth()
+
+    // handleRegister
+    const handleRegister = (e) => {
+        e.preventDefault()
+    }
+
     
     return ( 
         <>
             <div className="signup__form">
-                <label htmlFor="signup">Sign Up</label>
-                <input 
-                    type="text"
-                    placeholder="Enter Email"
-                    required
-                    onChange={(e) => setSignUpPassword(e.target.value)}
-                />
+                <form onSubmit={handleRegister}>
+                    <label htmlFor="signup">Sign Up</label>
+                    <input 
+                        type="text"
+                        placeholder="Enter Email"
+                        required
+                        onChange={(e) => setSignUpEmail(e.target.value)}
+                    />
 
-                <label htmlFor="password">Password</label>
-                <input 
-                    type="text"
-                    placeholder="Enter Password"
-                    required
-                    onChange={(e) => setSignUpPassword(e.target.value)}
-                />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="text"
+                        placeholder="Enter Password"
+                        required
+                        onChange={(e) => setSignUpPassword(e.target.value)}
+                    />
 
-                <button type="submit">Sign Up</button>
+                    <button type="submit">Sign Up</button>
 
-                <p>Already have an account? <Link to="login">Login</Link></p>
+                    <p>Already have an account? <Link to="login">Login</Link></p>
+                </form>              
             </div>
         </>
      );
