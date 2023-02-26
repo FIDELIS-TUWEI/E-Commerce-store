00800a49b76be258ef941fe134a8407d7eb4558f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-26 09:46:41 +0300

    homelayout css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,22 @@ import landing from '../assets/landing.jpeg'
 const Home = () => {
     return ( 
         <>
-            <div className="home__section">
-                <h2>Welcome to Fleekyffect</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
-                <Link to="signup">Get Started</Link>
-                <Link to="login">Login</Link>
-
+            <main className="home__section">
+                <section>
+                    <h2>Welcome to Fleekyffect</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
+                    <div className='links'>
+                        <Link to="signup">Get Started</Link>
+                        <Link to="login">Login</Link>
+                    </div>
+                    
+                </section>
+                
                 <div className="landing__image">
                     <img src={landing} alt="" />
                 </div>
 
-            </div>
+            </main>
             
             <footer>
                 <p>&copy; All rights reserved. Fleekyffect-2023</p>
