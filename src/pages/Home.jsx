c07a2749bee89e0108c183f236b7d37ef8640a6f
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-26 09:03:15 +0300

    landing image imported

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import landing from '../assets/landing.jpeg'
 
 const Home = () => {
     return ( 
@@ -7,6 +8,12 @@ const Home = () => {
                 <h2>Welcome to Fleekyffect</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
                 <Link to="signup">Get Started</Link>
+                <Link to="login">Login</Link>
+
+                <div className="landing__image">
+                    <img src={landing} alt="" />
+                </div>
+
             </div>
             
             <footer>
