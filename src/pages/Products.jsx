56d470c84491b5f04614a412bf789aadb9482416
bufleek <brunookiprop@gmail.com>
2023-02-27 00:15:23 +0300

    product display

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,10 @@
 import firebase, { getProducts } from '../Firebase'
 import { addDoc, deleteDoc, getFirestore, onSnapshot, updateDoc } from 'firebase/firestore';
-import  { collection, doc } from 'firebase/firestore'
+import { collection, doc } from 'firebase/firestore'
 import { useState, useEffect } from 'react';
 import Form from './Form'
+import { Box, Button, Card, Container, Grid, Icon, IconButton, Link, Stack, Typography, useTheme } from '@mui/material';
+import { ChevronRight, PlaylistAddCircleOutlined } from '@mui/icons-material';
 
 const Products = () => {
     // useState
@@ -30,11 +32,11 @@ const Products = () => {
         fetchProducts()
     }, []);
 
-    
+
     // Add New Product function
     const addProduct = (e) => {
         e.preventDefault()
-        addDoc(colRef, {name: newProduct, category: newCategory, price: Number(newPrice), image_url: image })
+        addDoc(colRef, { name: newProduct, category: newCategory, price: Number(newPrice), image_url: image })
         console.log('submit')
 
     }
@@ -44,7 +46,7 @@ const Products = () => {
         // document collection
         const docRef = doc(db, "jumia_products", id)
         // Increment price
-        const newPrice = {price: price + 1}
+        const newPrice = { price: price + 1 }
         //update price
         updateDoc(docRef, newPrice)
     }
@@ -57,58 +59,92 @@ const Products = () => {
         deleteDoc(docRef)
     }
 
-    return ( 
+    const theme = useTheme()
+
+    return (
         <>
-            <div className='container'>
-            <Form
-                addProduct={addProduct}
-                setNewProduct={setNewProduct}
-                setNewCategory={setNewCategory}
-                setNewPrice={setNewPrice}
-                setImage={setImage}
-            />
+            <Container>
+                <Grid mt={4} container columnSpacing={1} rowSpacing={3}>
+                    {
+                        products.map(product => {
+                            const hasDiscount = product.old_price || (product.lower_old_price_limit && product.upper_old_price_limit)
+                            return <Grid item xs={6} md={4} lg={3} key={product.id}>
+                                <Card>
+                                    <Stack>
+                                        <Stack sx={{ position: "relative" }}>
+                                            <img src={product.image_url} alt={product.name} />
+                                            <IconButton color="primary" size="large" sx={{
+                                                position: "absolute",
+                                                bottom: 0,
+                                                right: 0,
+                                                backgroundColor: "#ffffff"
+                                            }} 
+                                            aria-label="Add to catalog">
+                                                <PlaylistAddCircleOutlined fontSize="inherit" />
+                                            </IconButton>
+                                        </Stack>
 
-            <div id="catalog">
-                {products.map((item) => {
-                    return (
-                        <section key={item.id} className="prd">
-                                <div className='image__container'>
-                                    <img src={item.image_url} alt="" className='image' />
-                                </div>
+                                        <Box p={2}>
+                                            <Typography variant="body2" sx={{
+                                                overflow: "hidden",
+                                                textOverflow: "ellipsis",
+                                                display: "-webkit-box",
+                                                WebkitLineClamp: "1",
+                                                WebkitBoxOrient: "vertical",
+                                            }}> {product.name} </Typography>
 
-                                <div className='item__description'>
-                                    <h5>Brand: {item.name}</h5>
-                                
-                                    <p className='price'>
-                                    Ksh. {item.price}/= 
-                                    <span><a href={item.url} target="_blank" className='buy__item'>Buy item</a></span>
-                                    </p>
-                                    
-                                
-                                    <div className='buttons'>
-                                        <button
-                                            className='btn'
-                                            onClick={() => {updateItem(item.id, item.price)}}
-                                        >
-                                            Update Price
-                                        </button>
 
-                                        <button
-                                            className='btn'
-                                            onClick={() => {deleteItem(item.id)}}
-                                        >
-                                            Delete Item
-                                        </button>
-                                    </div>
-                                </div>           
-                        </section>
-                    )
-                })}
-            </div>
-            <button onClick={fetchProducts}>Load More</button>
-            </div>
+                                            <Typography
+                                                variant="body2"
+                                                mt={1}
+                                                sx={{
+                                                    textDecoration: hasDiscount ? "line-through" : "none"
+                                                }}
+                                                textAlign={hasDiscount ? "left" : "right"}
+                                                color={hasDiscount ? "inherit" : 'GrayText'}>
+                                                {!hasDiscount ? "No Discount" : "Ksh " + product.old_price || product.lower_old_price_limit && product.upper_old_price_limit}
+                                            </Typography>
+
+
+                                            <Stack direction="row" justifyContent="space-between">
+                                                <Typography variant="h6">Ksh {product.price}</Typography>
+                                                <Box>
+                                                    {
+                                                        product.percentage_discount ?
+                                                            <Typography
+                                                                textAlign="center"
+                                                                sx={{
+                                                                    textDecoration: "line-through"
+                                                                }}
+                                                                color="primary"> {product.percentage_discount}%</Typography> :
+                                                            <Box />
+                                                    }
+                                                </Box>
+                                            </Stack>
+
+                                            <Stack alignItems="center">
+                                                <Link
+                                                    href={product.url}
+                                                    color="inherit"
+                                                    underline="none"
+                                                    target="blank"
+                                                    rel="noreferrer">
+                                                    View In Jumia
+                                                </Link>
+                                            </Stack>
+                                        </Box>
+                                    </Stack>
+                                </Card>
+                            </Grid>
+                        }
+                        )
+                    }
+
+                </Grid>
+
+            </Container>
         </>
     );
 }
- 
+
 export default Products;
---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            // light
+            main: "#f68b1e"
+            // dark
+            // contrastText
+        }
+    }
+})
