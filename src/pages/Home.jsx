2e321b85951a3272bcca10539a358d9683a3d5b4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-27 11:52:22 +0300

    home-links css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
                     <h2>Welcome to Fleekyffect</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, voluptate impedit placeat, adipisci hic atque dignissimos nostrum quam ad, officia aut deserunt. Placeat nesciunt esse consequuntur, odit repellendus explicabo corporis.</p>
                     <div className='links'>
-                        <Link to="signup">Get Started</Link>
-                        <Link to="login">Login</Link>
+                        <Link to="signup" className='home__link'>Get Started</Link>
+                        <Link to="login" className='home__link'>Login</Link>
                     </div>
                     
                 </section>
