8296ec4ee968e8c267ded7b214440bc29f203f35
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-28 12:54:06 +0300

    navlink state onClick color

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom'
 import './rootlayout.css'
 
 const RootLayout = () => {
+    const [active, setActive] = useState("")
     return ( 
         <>
             <div className="root__layout">
@@ -9,11 +11,11 @@ const RootLayout = () => {
                     <h3>Fleekyffect</h3>
 
                     <nav className='navbar'>                     
-                        <NavLink to='/'>Home</NavLink>
-                        <NavLink to='login'>Login</NavLink>
-                        <NavLink to='signup'>SignUp</NavLink>
-                        <NavLink to='products'>Products</NavLink>
-                        <NavLink to='account'>Account</NavLink>
+                        <NavLink to='/' onClick={(e) => setActive} className={active ? "active" : ""}>Home</NavLink>
+                        <NavLink to='login' onClick={(e) => setActive} className={active ? "active" : ""}>Login</NavLink>
+                        <NavLink to='signup' onClick={(e) => setActive} className={active ? "active" : ""}>SignUp</NavLink>
+                        <NavLink to='products' onClick={(e) => setActive} className={active ? "active" : ""}>Products</NavLink>
+                        <NavLink to='account' onClick={(e) => setActive} className={active ? "active" : ""}>Account</NavLink>
                     </nav>
                 </header>
 
