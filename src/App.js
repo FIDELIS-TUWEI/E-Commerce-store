cb9695e673fc72d32a6f6cb4440ae7ea28b92bad
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-10 20:03:39 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css'
 import Form from './pages/common/Form';
 import Products from './pages/Products'
 import { ThemeProvider } from '@mui/material';
 import {theme} from './theme';
 import { useEffect, useState } from 'react';
-import app, { auth } from './Firebase'
 import {
   getAuth,
   createUserWithEmailAndPassword,
