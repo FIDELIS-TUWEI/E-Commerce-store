48dcf64b4d08c6163d233ac4b022e0c973430c94
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-10 19:58:10 +0300

    layout

---
+++ src/pages/common/layout/RootLayout.jsx
@@ -0,0 +1,20 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+    return ( 
+        <div>
+            <header>
+                <nav>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/register">Register</NavLink>
+                </nav>
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </div>
+     );
+}
+ 
+export default RootLayout;
