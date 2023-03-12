c43b22e63567f124ac5132ef3e902de1e8d9546a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-12 11:17:32 +0300

    Form changes with mui

--- src/pages/common/Form.jsx
+++ src/pages/common/Form.jsx
@@ -1,44 +1,99 @@
 import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 import Button from "./Button";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { FormControlLabel, Typography } from '@mui/material';
+import { CheckBox } from '@mui/icons-material';
+
+// ThemeProvider
+const theme = createTheme();
 
 const Form = ({title, setEmail, setPassword, handleAction}) => {
-    return ( 
-        <Grid container component="main" sx={{ height: '100vh' }}>
-                <h3>{title} Form</h3>
+    
 
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': {m: 1, width: '25ch'},                    
-                }}
-                noValidate
-                autoComplete="off"
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <TextField 
-                    id="email"
-                    label="Enter your Email"
-                    variant="outlined"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
+    return ( 
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) => 
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
                 />
-                <TextField
-                    id="password"
-                    label="Enter your Password"
-                    variant="outlined"
-                    type={"password"}
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button title={title} handleAction={handleAction} />
-            </Box>
-        </Grid >
+
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary-main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+
+                        <Typography>
+                            {title} Form
+                        </Typography>
+
+                        <Box
+                            component="form"
+                            sx={{ mt: 1 }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField
+                                margin='normal'
+                                fullWidth
+                                autoFocus 
+                                id="email"
+                                label="Enter your Email"
+                                variant="outlined"
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <TextField
+                                margin='normal'
+                                fullWidth
+                                autoFocus
+                                id="password"
+                                label="Enter your Password"
+                                variant="outlined"
+                                type={"password"}
+                                required
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            
+                            <FormControlLabel
+                                control={<CheckBox value='remember' color='primary' />}
+                                label='Remember me'
+                            />
+
+                            <Button title={title} handleAction={handleAction} />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid >
+        </ThemeProvider>
      );
 }
  
