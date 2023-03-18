262cc5e54f88c91ca30889c6c67daab533150753
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-18 09:57:10 +0300

    HeroLayoutRoot

--- src/layout/HeroLayout.jsx
+++ src/layout/HeroLayout.jsx
@@ -2,6 +2,19 @@ import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
 import { Container, Box } from "@mui/material";
 
+// HeroLayoutRoot
+const HeroLayoutRoot = styled('section')(({ theme }) => ({
+    color: theme.palette.common.white,
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+        height: '80vh',
+        minHeight: 500,
+        maxHeight: 1300,
+    }
+}));
+
 const HeroLayout = () => {
     return ( 
         <></>
