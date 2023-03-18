0ec663511372af5706d458b493f7b32ee04cd0ad
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-18 09:50:43 +0300

    Hero imports

---
+++ src/layout/HeroLayout.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import { Container, Box } from "@mui/material";
+
+const HeroLayout = () => {
+    return ( 
+        <></>
+     );
+}
+ 
+export default HeroLayout;
