634ad93a71332601be0b9d8b68f7eb3f1d1c6782
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-18 13:07:28 +0300

    HeroLayout done

--- src/layout/HeroLayout.jsx
+++ src/layout/HeroLayout.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
 import { Container, Box } from "@mui/material";
+import landing from '../assets/landing.jpg'
 
 // HeroLayoutRoot
 const HeroLayoutRoot = styled('section')(({ theme }) => ({
@@ -33,9 +34,49 @@ const HeroLayout = (props) => {
     const { sxBackground, children } = props;
     return ( 
         <HeroLayoutRoot>
-            <Container></Container>
+            <Container
+                sx={{
+                    mt: 3,
+                    mb: 14,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <img
+                    src={landing}
+                    alt=""
+                    width="147"
+                    height="80"
+                />
+                {children}
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0,
+                        backgroundColor: 'common.black',
+                        opacity: 0.5,
+                        zIndex: -1,
+                    }}
+                />
+                <Background sx={sxBackground}/>
+            </Container>
         </HeroLayoutRoot>
      );
 }
+
+HeroLayout.propTypes = {
+    children: PropTypes.node,
+    sxBackground: PropTypes.oneOfType([
+        PropTypes.arrayOf(
+            PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
+        ),
+        PropTypes.func,
+        PropTypes.object,
+    ]),
+};
  
 export default HeroLayout;
