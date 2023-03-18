470ae37221ebd86d292c97732c8d605038e1542a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-18 10:00:53 +0300

    Background MUI

--- src/layout/HeroLayout.jsx
+++ src/layout/HeroLayout.jsx
@@ -15,6 +15,19 @@ const HeroLayoutRoot = styled('section')(({ theme }) => ({
     }
 }));
 
+// Background
+const Background = styled(Box)({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    zIndex: -2,
+});
+
+// HeroLayout
 const HeroLayout = () => {
     return ( 
         <></>
