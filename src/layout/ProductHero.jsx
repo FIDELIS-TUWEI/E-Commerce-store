a73ef3a73d01aeda79eeef9b7890d1dfcf776836
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-19 17:03:31 +0300

    bug fix

--- src/layout/ProductHero.jsx
+++ src/layout/ProductHero.jsx
@@ -1,9 +1,8 @@
 import Typography from "@mui/material/Typography";
 import HeroLayout from "./HeroLayout";
-import landing from '../assets/landing.jpg'
 
 // background image
-const backgroundImage = {landing}
+const backgroundImage = 'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
 
 const ProductHero = () => {
     return ( 
