a259c76e1f5beb5db1a6289b270eeab741b9c41b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-19 16:48:09 +0300

    Background image added

---
+++ src/layout/ProductHero.jsx
@@ -0,0 +1,18 @@
+import Typography from "@mui/material/Typography";
+import HeroLayout from "./HeroLayout";
+
+// background image
+const backgroundImage = 'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
+
+const ProductHero = () => {
+    return ( <HeroLayout>
+        <img
+            style={{ display: 'none' }}
+            src={backgroundImage}
+            alt=""
+        />
+    </HeroLayout> 
+    );
+}
+ 
+export default ProductHero;
