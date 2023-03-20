4bae25998c3f8c68ebafdd71b451cc8fbb56963e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-20 10:40:35 +0300

    bug fix

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -4,9 +4,6 @@ import ProductHero from "./ProductHero";
 const RootLayout = () => {
     return ( 
         <div>
-            <main>
-                <Outlet />
-            </main>
             <ProductHero/>
         </div>
      );
