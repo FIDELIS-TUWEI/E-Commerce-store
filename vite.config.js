cf8b0601967ff26688c7c7741ac65a9595708c0b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-01-24 12:57:08 +0300

    Output build settings added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
     plugins: [react()],
+  };
 });
