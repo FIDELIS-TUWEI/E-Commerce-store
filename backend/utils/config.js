cd2754b89c582cd31b9df1901692732e6cc0cb5a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-09 20:43:12 +0300

    Redis environment variables set

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 let MONGODB_URI = process.env.MONGODB_URI;
 let PORT = process.env.PORT;
+let UPSTASH_REDIS_URL = process.env.UPSTASH_REDIS_URL;
 
 module.exports = {
-    MONGODB_URI, PORT
+    MONGODB_URI, PORT, UPSTASH_REDIS_URL
 }
