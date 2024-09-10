af95142fb3c09ad7d0aed7b75bd0ce0c3fe73080
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-10 12:28:38 +0300

    cookieparser middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const appRoutes = require("./routes/index");
 
 const app = express();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true })); // parse form data(urlencoded)
+app.use(cookieParser());
 
 app.disable("x-powered-by"); // disable express server fingerprinting
 
