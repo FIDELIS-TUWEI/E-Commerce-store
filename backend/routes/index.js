d9f80006edf65410f18d3911a7b14fe7847fc26d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-11 14:42:06 +0300

    product routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./auth.routes");
+const productRoutes = require("./product.routes");
 
 router.use("/auth", authRoutes);
+router.use("/products", productRoutes);
 
 router.get("/favicon.ico", (req, res) => res.status(204)); // Respond with no content (status 204)
 
