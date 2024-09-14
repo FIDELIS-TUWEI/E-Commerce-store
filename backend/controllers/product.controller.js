5560f7baee3213a5ac4feb35d025dfb53900dc37
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-14 12:48:46 +0300

    get featured products controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../model/product.model");
 const logger = require("../utils/logger");
+const redis = require("../utils/redis");
 
 const getAllProducts = async (req, res) => {
     try {
@@ -12,4 +13,29 @@ const getAllProducts = async (req, res) => {
     }
 };
 
-module.exports = { getAllProducts };
+const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts))
+        };
+
+        // if not in redis fetch from mongodb
+        // .lean()  method is going to return a plain javascript object instead of a mongodb document
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        if (!featuredProducts) {
+            return res.status(404).json({ status: "error", message: "No featured products found" });
+        };
+
+        // store in redis for future quick access
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+        res.json(featuredProducts);
+    } catch (error) {
+        logger.error("Error occurred on getFeaturedProducts controller:", error);
+        res.status(500).json({ status: "error", message: error.message || "Internal Server Error" });
+    }
+}
+
+module.exports = { getAllProducts, getFeaturedProducts };
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/product.controller");
+const { getAllProducts, getFeaturedProducts } = require("../controllers/product.controller");
 const {protectRoute, adminRoute} = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 module.exports = router;
