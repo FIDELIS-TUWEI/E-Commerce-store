3cd489c999d1627a445d9788e63001094ac237bf
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-09-15 14:25:48 +0300

    create, delete product controllers

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../model/product.model");
 const logger = require("../utils/logger");
 const redis = require("../utils/redis");
+const cloudinary = require("../lib/cloudinary");
 
 const getAllProducts = async (req, res) => {
     try {
@@ -36,6 +37,58 @@ const getFeaturedProducts = async (req, res) => {
         logger.error("Error occurred on getFeaturedProducts controller:", error);
         res.status(500).json({ status: "error", message: error.message || "Internal Server Error" });
     }
+};
+
+const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, image, category } = req.body;
+
+        let cloudinaryResponse = null;
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+        };
+
+        const products = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category
+        });
+
+        res.status(201).json({ status: "success", message: "Product created successfully", products });
+    } catch (error) {
+        logger.error("Error occurred on createProduct controller:", error);
+        res.status(500).json({ status: "error", message: error.message || "Internal Server Error" });
+    }
 }
 
-module.exports = { getAllProducts, getFeaturedProducts };
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ status: "error", message: "Product not found" });
+        };
+
+        if (product.image) {
+            const publicId = product.image.split("/").pop().split(".")[0];
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+                logger.info("Image deleted from cloudinary successfully");
+            } catch (error) {
+                logger.error("Error deleting image from cloudinary:", error);
+            };
+        };
+
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.json({ status: "success", message: "Product deleted successfully" });
+    } catch (error) {
+        logger.error("Error occurred on deleteProduct controller:", error);
+        res.status(500).json({ status: "error", message: error.message || "Internal Server Error" });
+    }
+};
+
+module.exports = { getAllProducts, getFeaturedProducts, createProduct, deleteProduct };
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAllProducts, getFeaturedProducts } = require("../controllers/product.controller");
+const { getAllProducts, getFeaturedProducts, createProduct, deleteProduct } = require("../controllers/product.controller");
 const {protectRoute, adminRoute} = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 module.exports = router;
